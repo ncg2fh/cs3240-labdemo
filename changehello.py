39db9dd52dcfe60728c1b7cdfc68110ea007fe14
Nicholas Georgiou <ncg2fh@virginia.edu>
2017-09-11 17:55:20 -0400

    I added more exclamations!

--- changehello.py
+++ changehello.py
@@ -1,4 +1,4 @@
 #Nicholas Georgiou (ncg2fh), CS3240 Lab 3
 
 print("My name is Nick")
-print("This is my favorite time of the year! Totally awesome!")
+print("This is my favorite time of the year! Totally awesome!!!")
